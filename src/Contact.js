db67068a4f3a870938bcca8997488295edc25df0
Caldewood Alikula <alikulaowen@gmail.com>
2022-06-18 11:23:36 +0300

    #fixed emailjs

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,7 @@
 
 import './Contact.css';
 import contactImg from "./img/logoa.png"
-import React, { useRef } from 'react';
+import React, { useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
 
 
@@ -9,9 +9,13 @@ import emailjs from '@emailjs/browser';
 
 export default function Contact() {
     const form = useRef();
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [message, setMsg] = useState();
 
     const sendEmail = (e) => {
         e.preventDefault();
+        alert(`Hey ${name}, Your message has been sent to BrainTech`)
 
         emailjs.sendForm('service_i2g4vxi', 'template_yh4fjwe', form.current, '30lwwBmPXC8iilHvm')
             .then((result) => {
@@ -33,12 +37,12 @@ export default function Contact() {
                         {/*  */}
                         <form ref={form} onSubmit={sendEmail}>
                             <div className="input__box">
-                            <label>Name</label>
-                            <input type="text" name="user_name"  className="contact name" placeholder="Your name *"  />
+                            <label>Name
+                            <input type="text" name="user_name"  onChange={e => setName(e.target.value)} value={name} className="contact name" placeholder="Your name *"  /> </label>
                             <label>Email</label>
-                            <input type="email" name="user_email" className="contact email" placeholder="Your Email *" />
+                            <input type="email" name="user_email" onChange={e => setEmail(e.target.value)} value={email}  className="contact email" placeholder="Your Email *" />
                             <label>Message</label>
-                            <textarea name="message" value='message'className="contact email" placeholder="Your Message *" />
+                            <textarea name="message" onChange={e => setMsg(e.target.value)} value={message} className="contact_message_box" placeholder="Your Message *" />
                             <button type="submit" value="Send" className="btn contact pointer" >Send</button>
                              </div>
                         </form>
